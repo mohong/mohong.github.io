fb5751deb9a108037f14beb17aadaabca5b2ce99
mohong <arthas_qin@qq.com>
2021-12-25 01:19:26 +0800

    Updates

--- baidu.js
+++ baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?152cb168941799d8bdb7e34a71468eda";
+  hm.src = "https://hm.baidu.com/hm.js?3e113b493e4948705b7d0ae8e7d4039d";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
